77ae49c12c56d5ae1a5a50feae7c9794daa07b0e
Danish Khan <khandanish78866@gmail.com>
2024-11-23 21:50:13 +0530

    baseURL updated

--- src/utils/apiClient.ts
+++ src/utils/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:5000/api", // Backend API base URL
+  baseURL: "https://smart-delivery-management-backend.onrender.com/api", // Backend API base URL
   headers: {
     "Content-Type": "application/json",
   },
